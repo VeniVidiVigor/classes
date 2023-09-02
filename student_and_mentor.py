f117c225877c4c187b41e55210d221a583e668bf
Kirill Veni <VeniVidiVigorY@yandex.ru>
2023-09-02 22:46:58 +0300

    Zadanie 2

--- student_and_mentor.py
+++ student_and_mentor.py
@@ -6,7 +6,17 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-            
+
+    def get_feedback(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached \
+                and course in self.courses_in_progress and 0 < grade <= 10:
+            if course not in lecturer.feedback:
+                lecturer.feedback[course] = [grade]
+            else:
+                lecturer.feedback[course].append(grade)
+        else:
+            print("Некорректная оценка")
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,10 +26,24 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    feedback_list = []
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.feedback = {}
+        Lecturer.feedback_list.append(self)
+
+
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
